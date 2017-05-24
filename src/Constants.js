3030c7afc992e2b9906fc8741be0014cf279da99
Benoit Prioux <benoit.prioux@gmail.com>
2017-05-24 23:24:14 +0200

    use production url

--- src/Constants.js
+++ src/Constants.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080";
-//const BASE_URL = 'https://pes5.herokuapp.com';
+//const BASE_URL = "http://localhost:8080";
+const BASE_URL = 'https://pes5.herokuapp.com';
 
 export default BASE_URL;
